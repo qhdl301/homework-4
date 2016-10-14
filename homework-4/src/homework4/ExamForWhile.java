f01d56bf9321308ee1ce978f1061287ef3942743
Young soo Kim <qhdl301@gmail.com>
2016-10-14 12:15:38 +0900

    ctrl+shift+F 로 보기 편하게 수정!!

--- homework-4/src/homework4/ExamForWhile.java
+++ homework-4/src/homework4/ExamForWhile.java
@@ -1,53 +1,50 @@
 package homework4;
+
 import java.util.*;
+
 public class ExamForWhile {
-	
-	
 
 	public static void main(String[] args) {
 		int num;
 
-		Scanner s=new Scanner(System.in);
+		Scanner s = new Scanner(System.in);
 
-	    do{
+		do {
 
-	    	System.out.println("1. 1부터 입력한 수까지 더하기");
+			System.out.println("1. 1부터 입력한 수까지 더하기");
 
-	    	System.out.println("2. 최대값/최소값구하기");
+			System.out.println("2. 최대값/최소값구하기");
 
-	    	System.out.println("3. 입력받은숫자의 합계와 평균구하기");
+			System.out.println("3. 입력받은숫자의 합계와 평균구하기");
 
-	    	System.out.println("4. 원하는 구구단 출력하기  ");
+			System.out.println("4. 원하는 구구단 출력하기  ");
 
-	    	System.out.println("5. 짝수단/홀수단 출력하기");
+			System.out.println("5. 짝수단/홀수단 출력하기");
 
-	    	System.out.println("6. 종료하기");
+			System.out.println("6. 종료하기");
 
-	    	System.out.println("원하는 메뉴는>>");
-	    	num=s.nextInt();
-	    		if(num==1)
-	    			new SumOfNumbers().input();
-	    		else if(num==2)
+			System.out.println("원하는 메뉴는>>");
+			num = s.nextInt();
+			if (num == 1)
+				new SumOfNumbers().input();
+			else if (num == 2)
 
-	    			new MaxAndMin().input();
-	    		else if(num==3)
+				new MaxAndMin().input();
+			else if (num == 3)
 
-	    			new SumAndAvg().input();	
+				new SumAndAvg().input();
 
-	    		else if(num==4)
+			else if (num == 4)
 
-	    			new GuGuDan().input();
+				new GuGuDan().input();
 
-	    		else if(num==5)
+			else if (num == 5)
 
-	    			new GuGuDan2().input();
-	    }
-	    while(num!=6);
-	    System.out.print("종료하셨습니다.");
+				new GuGuDan2().input();
+		} while (num != 6);
+		System.out.print("종료하셨습니다.");
 	}
 
-		// TODO Auto-generated method stub
+	// TODO Auto-generated method stub
 
-	}
-
-
+}
--- homework-4/src/homework4/SumOfNumbers.java
+++ homework-4/src/homework4/SumOfNumbers.java
@@ -1,35 +1,33 @@
 package homework4;
+
 import java.util.*;
+
 public class SumOfNumbers {
-	Scanner s= new Scanner(System.in);
+	Scanner s = new Scanner(System.in);
 	int num;
 	int num1;
 	String queqe;
-	void input()
-	{
-		do{
-			this.queqe="";
-			this.num1=0;
-		System.out.println("마지막수를 입력(q입력종료):");
-		if(s.hasNextInt()){
-		num=s.nextInt();
-		print();
-		}
-		else {
-			this.queqe=s.next();
-			
-		}
-		}
-		while(!queqe.equals("q"));
+
+	void input() {
+		do {
+			this.queqe = "";
+			this.num1 = 0;
+			System.out.println("마지막수를 입력(q입력종료):");
+			if (s.hasNextInt()) {
+				num = s.nextInt();
+				print();
+			} else {
+				this.queqe = s.next();
+
+			}
+		} while (!queqe.equals("q"));
 	}
 
-	void print()
-	{
-		
-			for(int i=1; i<=num; i++)
-			{
-				num1=num1+i;
-			}
-			System.out.println("결과는"+num1);
+	void print() {
+
+		for (int i = 1; i <= num; i++) {
+			num1 = num1 + i;
 		}
+		System.out.println("결과는" + num1);
+	}
 }
